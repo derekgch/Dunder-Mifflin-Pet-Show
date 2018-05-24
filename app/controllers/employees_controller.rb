5e075646d72512f42056845f4f5add55e30888af
Canino Developments <ian.canino@gmail.com>
2018-05-24 14:56:50 -0400

    Done driving with you all!

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,12 +7,23 @@ before_action :get_employee, only: [:show]
 
   def show
   end
+  
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
 
+  def create
+    @employee = Employee.create(getparams)
+  end
   private
 
   def get_employee
     @employee = Employee.find(params[:id])
   end
 
+  def getparams
+    params.require(:employee).permit(:first_name, :last  , :office, :alias, )
+  end
 
 end
