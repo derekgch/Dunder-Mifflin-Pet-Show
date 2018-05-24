79ed547fad4c6b5986586fcc45e7556a8a18c783
github username <torpid@gmail.com>
2018-05-24 14:38:49 -0400

    me

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,18 @@
 class DogsController < ApplicationController
+before_action :get_dog, only: [:show]
+
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+  end
+
+  private
+
+  def get_dog
+    @dog = Dog.find(params[:id])
+  end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,18 @@
 class EmployeesController < ApplicationController
+before_action :get_employee, only: [:show]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  private
+
+  def get_employee
+    @employee = Employee.find(params[:id])
+  end
+
+
 end
